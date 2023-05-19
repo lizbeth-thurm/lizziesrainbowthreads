e89bca67bc3ca26aeab77fbe30d8142f17244d7a
slthurman01 <slthurman01@gmail.com>
2023-05-19 10:18:08 -0500

    Works out image on page, edits to sections

--- src/App.js
+++ src/App.js
@@ -1,18 +1,43 @@
 import "./App.css";
 import React from "react";
 import Header from "./components/Header";
+import pride_flag from "./pride_flag.png";
 
 function App() {
   return (
     <div className="App">
       <Header />
       <div className="App-main">
-        <h1>Lizzie's Rainbow Threads</h1>
-        <h3>Pride-Themed Accessories and Apparel</h3>
-        <h3>Custom Tailoring and Alterations Specializing in Clothes for Trans People!</h3>
-        <h2 className="about">ABOUT</h2>
-        <h2 className="social">SOCIAL</h2>
-        <h2 className="contact">CONTACT</h2>
+        <div className="Home-intro">
+          <h1>Lizzie's Rainbow Threads</h1>
+          <h4>LIBERATION THROUGH FASHION</h4>
+          <img src={pride_flag} className="pride_flag" alt="Gay Pride Flag" />
+          <h3>Pride-Themed Accessories and Apparel - Custom Tailoring and Alterations Specializing in Clothes for Trans
+            People!</h3>
+          <h2 className="about" id="about">
+            ABOUT
+          </h2>
+          <p>Hello and welcome to Lizzie's Rainbow Threads!</p>
+          <p>My name is Lizzie (She/Her) - I am a seamstress from Arkansas and my passion is fueling queer joy!</p>
+        </div>
+        <h2 className="social" id="social">
+          SOCIAL
+        </h2>
+        <p>
+<ul>
+  <li>Twitter</li>
+  <li>Instagram</li>
+  <li>Facebook</li>
+  <li>LinkedIn</li>
+  <li>Etsy</li>
+</ul>
+        </p>
+        <h2 className="contact" id="contact">
+          CONTACT
+        </h2>
+        <div className="email_add">
+          <a href="mailto:lizzie@lizziesrainbowthreads.com">Email</a>
+        </div>
       </div>
     </div>
   );
--- src/components/Header.js
+++ src/components/Header.js
@@ -2,18 +2,33 @@ import React from "react";
 import useScrollDirection from "../components/Scroller";
 import logo from "../LOGO.jpg";
 import "./Components.css";
+import { Link } from "react-scroll";
 
 function Header() {
-    const scrollDirection = useScrollDirection();
-  
+  const scrollDirection = useScrollDirection();
+
   return (
-    <div className={`App-header ${ scrollDirection === "down" ? "hide" : "show"}`}>
-        <img src={logo} className="App-logo" alt="logo" />
-        <h2 className="Header-1">ABOUT</h2>
-        <h2 className="Header-2">SOCIAL</h2>
-        <h2 className="Header-3">CONTACT</h2>
+    <div
+      className={`App-header ${scrollDirection === "down" ? "hide" : "show"}`}
+    >
+      <img src={logo} className="App-logo" alt="logo" />
+      <h2 className="Header-1">
+        <Link to="about" spy={true} smooth={true}>
+          ABOUT
+        </Link>
+      </h2>
+      <h2 className="Header-2">
+        <Link to="social" spy={true} smooth={true}>
+          SOCIAL
+        </Link>
+      </h2>
+      <h2 className="Header-3">
+        <Link to="contact" spy={true} smooth={true}>
+          CONTACT
+        </Link>
+      </h2>
     </div>
   );
-};
+}
 
 export default Header;
